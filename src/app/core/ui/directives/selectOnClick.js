20b963015b927f5f23d691ddb78aa2ccdfd6a573
gfreeau <greg@imagize.com.au>
2014-09-20 14:53:24 -0400

    Added directive to select input on click

---
+++ src/app/core/ui/directives/selectOnClick.js
@@ -0,0 +1,14 @@
+angular.module('tagcade.core.ui')
+
+    .directive('selectOnClick', function () {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                element.on('click', function () {
+                    this.select();
+                });
+            }
+        };
+    })
+
+;
