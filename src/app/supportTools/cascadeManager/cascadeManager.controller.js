2b0e9c65a247c8caffb0511ea7fa3a12215327d4
litpuvn <litpuvn@gmail.com>
2015-01-09 17:52:38 +0700

    add all sites option for cascade manager

--- src/app/supportTools/cascadeManager/cascadeManager.controller.js
+++ src/app/supportTools/cascadeManager/cascadeManager.controller.js
@@ -10,28 +10,64 @@
         $scope.adNetworks = adNetworks;
         $scope.sites = null;
 
+        $scope.addAllOption = addAllOption;
+
         $scope.selected = {
             adNetwork : null,
-            site : null,
             position: null
         };
 
-        $scope.selectAdNetwork = function (publisher, publisherId) {
-            $scope.selected.site = null;
-            return AdNetworkManager.one(publisherId).one('sites').one('active').getList().then(function (sites) {
-                $scope.sites = sites.plain();
+        /**
+         *
+         * @param {Array} data
+         * @param {String} [label]
+         * @returns {Array}
+         */
+        function addAllOption(data, label)
+        {
+            if (!angular.isArray(data)) {
+                throw new Error('Expected an array of data');
+            }
+
+            data.unshift({
+                id: null, // default value
+                name: label || 'All'
             });
+
+            return data;
+        }
+
+        $scope.groupEntities = function(item){
+            if (item.id === null) {
+                return undefined; // no group
+            }
+
+            return ''; // separate group with no name
+        };
+
+        $scope.selectAdNetwork = function (adNetworkId) {
+            return AdNetworkManager.one(adNetworkId).one('sites').one('active').getList()
+                    .then(function (data) {
+                        addAllOption(data, 'All Site');
+                        $scope.sites = data.plain();
+                    })
+                ;
         };
 
         $scope.isFormValid = function() {
-            return  $scope.selected.adNetwork != null &&
-                    $scope.selected.site != null &&
-                    $scope.selected.position != null;
+            return $scope.selected.adNetwork != null && $scope.selected.position != null;
         };
 
         $scope.submit = function () {
+            var savePosition;
 
-            var savePosition = AdNetworkManager.one($scope.selected.adNetwork).one('sites', $scope.selected.site).customPUT('', 'position', {position: $scope.selected.position});
+            if(!$scope.selected.site) {
+                savePosition = AdNetworkManager.one($scope.selected.adNetwork).customPUT('', 'position', {position: $scope.selected.position});
+            }
+            else {
+                savePosition = AdNetworkManager.one($scope.selected.adNetwork).one('sites', $scope.selected.site).customPUT('', 'position', {position: $scope.selected.position});
+            }
+
             savePosition
                 .then(
                 function () {
