82f9beef5f235ba3a1e73bb23b5398c03c8cd41c
litpuvn <litpuvn@gmail.com>
2015-01-22 14:29:16 +0700

    ignore today on default date range

--- src/app/reports/performance/controllers/performanceReportSelector.controller.js
+++ src/app/reports/performance/controllers/performanceReportSelector.controller.js
@@ -63,8 +63,8 @@
             ranges: {
                 'Today': [moment().startOf('day'), moment().endOf('day')],
                 'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                'Last 7 Days': [moment().subtract(7, 'days'), moment().subtract(1, 'days')],
+                'Last 30 Days': [moment().subtract(30, 'days'), moment().subtract(1, 'days')],
                 'This Month': [moment().startOf('month'), moment().endOf('month')],
                 'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
             }
@@ -375,7 +375,7 @@
             update();
 
             if (!$scope.selectedData.date.startDate) {
-                $scope.selectedData.date.startDate = moment().startOf('day');
+                $scope.selectedData.date.startDate = moment().subtract(1, 'days').startOf('day');
             }
 
             if (!$scope.selectedData.date.endDate) {
