770bad287a2f814543fe765dd7e3b4080ea02906
HieuTran <dtag.dev1@gmail.com>
2015-06-08 17:09:04 +0700

    show event counter in report summary

--- src/app/reports/performance/controllers/performanceReportSummary.controller.js
+++ src/app/reports/performance/controllers/performanceReportSummary.controller.js
@@ -5,11 +5,18 @@
         .controller('PerformanceReportSummary', PerformanceReportSummary)
     ;
 
-    function PerformanceReportSummary($scope) {
+    function PerformanceReportSummary($scope, Auth) {
+        $scope.isAdmin = Auth.isAdmin();
+
         $scope.hasSlotOpportunities = hasSlotOpportunities;
+        $scope.showItem = showItem;
 
         function hasSlotOpportunities() {
             return angular.isObject($scope.reportGroup) && angular.isNumber($scope.reportGroup.slotOpportunities);
+        }
+
+        function showItem() {
+            return angular.isNumber($scope.reportGroup.clicks);
         }
     }
 })();
