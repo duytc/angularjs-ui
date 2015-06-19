6e648de6aa896a6c3da0f3002744f451db80bf90
litpuvn <litpuvn@gmail.com>
2015-06-19 12:47:29 +0700

    support select on click for div, textarea and input tag

--- src/app/core/widgets/selectOnClick.directive.js
+++ src/app/core/widgets/selectOnClick.directive.js
@@ -10,7 +10,21 @@
             restrict: 'A',
             link: function (scope, element, attrs) {
                 element.on('click', function () {
-                    this.select();
+                    if (this.tagName == 'TEXTAREA' || this.tagName == 'INPUT') {
+                        this.select();
+                    }
+                    else {
+                        if (document.selection) {
+                            var range = document.body.createTextRange();
+                            range.moveToElementText(this);
+                            range.select();
+                        } else if (window.getSelection()) {
+                            var range = document.createRange();
+                            range.selectNode(this);
+                            window.getSelection().removeAllRanges();
+                            window.getSelection().addRange(range);
+                        }
+                    }
                 });
             }
         };
