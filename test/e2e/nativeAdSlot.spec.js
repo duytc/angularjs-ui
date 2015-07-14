8b0c4682f3c5dd47c5b78c1a18625b08dd7de00f
dunght160387 <dunght1603@gmail.com>
2015-07-14 11:51:48 +0700

    add test spec for native ad slot. Also update test config.

---
+++ test/e2e/nativeAdSlot.spec.js
@@ -0,0 +1,89 @@
+describe("Native Ad Slots Test", function() {
+
+    var tagcadeTestUtil = require('./util/tagcade.test.util');
+
+    var name = element(by.model('adSlot.name'));
+
+    beforeEach(function () {
+    });
+
+    afterEach(function () {
+        if (this.results_.failedCount > 0) {
+            browser.quit();
+        }
+    });
+
+    it('should create Native Ad Slots successfully', function() {
+        browser.get(browser.params.role + '/tagManagement/adSlots/new');
+
+        selectAdSlotTypeNative();
+
+        selectPublisher(0);
+
+        selectSite(0);
+
+        name.sendKeys('nativeAdSlot-test' + tagcadeTestUtil.getDateString(new Date()));
+
+        element(by.css('.btn.btn-success')).click();
+
+        expect(element(by.binding('message')).getText()).
+            toEqual('The ad slot has been created');
+    });
+
+    it('should create Native Ad Slots successfully although too long name', function() {
+        browser.get(browser.params.role + '/tagManagement/adSlots/new');
+
+        selectAdSlotTypeNative();
+
+        selectPublisher(0);
+
+        selectSite(0);
+
+        name.sendKeys('nativeAdSlot-test' + tagcadeTestUtil.getDateString(new Date()) + '-qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertFFFFFFFFFF');
+
+        element(by.css('.btn.btn-success')).click();
+
+        //expect(element(by.binding('message')).getText()).
+        //    toEqual('An error occurred. The ad slot could not be created');
+        expect(element(by.binding('message')).getText()).
+            toEqual('The ad slot has been created');
+    });
+
+    it('should update Native Ad Slots successfully', function() {
+        browser.get(browser.params.role + '/tagManagement/adSlots/edit/' + browser.params.edit.nativeAdSlotId);
+
+        name.clear();
+        name.sendKeys('nativeAdSlot-test' + tagcadeTestUtil.getDateString(new Date()));
+
+        element(by.css('.btn.btn-success')).click();
+
+        expect(element(by.binding('message')).getText()).
+            toEqual('The ad slot has been updated');
+    });
+
+    it('should update Native Ad Slots successfully although too long name', function() {
+        browser.get(browser.params.role + '/tagManagement/adSlots/edit/' + browser.params.edit.nativeAdSlotId);
+
+        name.clear();
+        name.sendKeys('nativeAdSlot-test' + tagcadeTestUtil.getDateString(new Date()));
+
+        element(by.css('.btn.btn-success')).click();
+
+        //expect(element(by.binding('message')).getText()).
+        //    toEqual('An error occurred. The ad slot could not be updated');
+        expect(element(by.binding('message')).getText()).
+            toEqual('The ad slot has been updated');
+    });
+
+    function selectAdSlotTypeNative() {
+        tagcadeTestUtil.selectAdSlotTypeNative('selected.type');
+    }
+
+    function selectPublisher(rowId) {
+        tagcadeTestUtil.selectItemByModel('selected.publisher', '.ui-select-choices-row-inner', rowId);
+    }
+
+    function selectSite(rowId) {
+        tagcadeTestUtil.selectItemByModel('adSlot.site', '.ui-select-choices-row-inner', rowId);
+    }
+});
