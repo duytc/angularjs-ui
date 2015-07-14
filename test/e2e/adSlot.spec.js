2b1907018aad34ab12899d6d6b303591cc42a474
dunght160387 <dunght1603@gmail.com>
2015-07-14 16:45:27 +0700

    only get list ad slots for test spec adSlot

--- test/e2e/adSlot.spec.js
+++ test/e2e/adSlot.spec.js
@@ -1,42 +1,18 @@
 describe("Ad Slots Test", function() {
-    it('Ad Slots update', function() {
 
-        browser.get(browser.params.role + '/tagManagement/adSlots/edit/' + browser.params.edit.adSlotId);
-
-        var name = element(by.model('adSlot.name'));
-        var width = element(by.model('adSlot.width'));
-        var height = element(by.model('adSlot.height'));
-
-        name.clear();
-        width.clear();
-        height.clear();
-
-        name.sendKeys('adSlot-test');
-        width.sendKeys('100');
-        height.sendKeys('200');
-
-        element(by.css('.btn.btn-success')).click();
-
-        expect(element(by.binding('message')).getText()).
-            toEqual('The ad slot has been updated');
+    beforeEach(function () {
     });
 
-    it('native Ad Slots new', function() {
+    afterEach(function () {
+        if (this.results_.failedCount > 0) {
+            browser.quit();
+        }
+    });
 
-        browser.get(browser.params.role + '/tagManagement/adSlots/new?siteId=' + browser.params.edit.siteId);
+    it('list Ad Slots for menu item My AdSlot', function() {
+        browser.get(browser.params.role + '/tagManagement/adSlots/list');
 
-        element(by.model('selected.type')).click();
-        element.all(by.css('.ui-select-choices-row')).then(function(items) {
-            items[1].click()
-        });
-
-        var name = element(by.model('adSlot.name'));
-        name.clear();
-        name.sendKeys('adSlot-test-new');
-
-        element(by.css('.btn.btn-success')).click();
-
-        expect(element(by.binding('message')).getText()).
-            toEqual('The ad slot has been created');
+        expect(browser.getTitle()).
+            toEqual('Ad Slots | Tagcade Platform');
     });
 });
