4db049ce68aa5bbe056dd082e3765048537dd1c9
Hieu Tran <dtag.dev1@gmail.com>
2015-09-01 10:50:50 +0700

    add bower highlight.js

--- build/config/appConfig.js
+++ build/config/appConfig.js
@@ -68,7 +68,6 @@ module.exports = {
             'vendor/codemirror/mode/javascript/javascript.js',
             'vendor/codemirror/mode/htmlmixed/htmlmixed.js',
             'vendor/angular-highlightjs/src/angular-highlightjs.js',
-            'vendor/highlight.js/src/highlight.js',
             'vendor/highlightjs/highlight.pack.js',
             'vendor/zeroclipboard/dist/ZeroClipboard.min.js',
             'vendor/ng-clip/src/ngClip.js',
