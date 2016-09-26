1730323745eabed8cb3a48f03df4a81b3fb3c64a
duytc <tranduy8683@gmail.com>
2016-09-26 18:33:57 +0700

    add string active bulk

--- src/app/core/language/storage/locale-en.js
+++ src/app/core/language/storage/locale-en.js
@@ -375,6 +375,7 @@
                 "NEW_DEMAND_PARTNER": "New Demand Partner",
                 "SELECT_A_VIDEO_DEMAND_PARTNER": "Select a demand partner",
                 "PAUSE_DEMAND_AD_TAG_FOR_WATERFALL": "Bulk Pause",
+                "ACTIVE_DEMAND_AD_TAG_FOR_WATERFALL": "Bulk Active",
                 "CURRENTLY_NO_WATERFALLS_DEMAND_PARTNER": "Demand Partner : ' {{ demand_partner_name }} ' does not have waterfall tags",
                 "ACTIVE_DEMAND_AD_TAGS_COUNT": "Active Demand Ad Tags",
                 "PAUSE_DEMAND_AD_TAGS_COUNT": "Paused Demand Ad Tags",
