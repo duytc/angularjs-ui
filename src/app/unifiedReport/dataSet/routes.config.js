27dc6f3250821911993937d855173f0433d64d54
hieutran <dtag.dev1@gmail.com>
2017-04-26 15:16:27 +0700

    hot fix sort order default of data source and data set

--- src/app/unifiedReport/dataSet/routes.config.js
+++ src/app/unifiedReport/dataSet/routes.config.js
@@ -31,7 +31,7 @@
                 resolve: {
                     dataSets: /* @ngInject */ function(UnifiedReportDataSetManager, $stateParams) {
                         $stateParams.page = !$stateParams.page ? 1 : $stateParams.page;
-                        $stateParams.orderBy = !$stateParams.orderBy ? 'desc' : $stateParams.page;
+                        $stateParams.orderBy = !$stateParams.orderBy ? 'desc' : $stateParams.orderBy;
                         $stateParams.sortField = !$stateParams.sortField ? 'lastActivity' : $stateParams.sortField;
 
                         return UnifiedReportDataSetManager.one().get($stateParams).then(function (dataSets) {
