fd9ee327230921ef12c1c7b049cd47071e2f4d76
hieutran <dtag.dev1@gmail.com>
2017-11-07 16:51:50 +0700

    remove macros waterfall_id & demand_tag_id

--- src/app/videoManagement/IVTPixel/config.js
+++ src/app/videoManagement/IVTPixel/config.js
@@ -3,8 +3,8 @@
 
     angular.module('tagcade.videoManagement.IVTPixel')
         .constant('IVT_PIXEL_MACROS', [
-            {label: "waterfall_id", key: "waterfall_id", root: '${waterfall_id}', helpText: 'Unique id of the Video Waterfall Tag'},
-            {label: "demand_tag_id", key: "demand_tag_id", root: '${demand_tag_id}', helpText: 'Unique id of the Video Demand Tag'},
+            // {label: "waterfall_id", key: "waterfall_id", root: '${waterfall_id}', helpText: 'Unique id of the Video Waterfall Tag'},
+            // {label: "demand_tag_id", key: "demand_tag_id", root: '${demand_tag_id}', helpText: 'Unique id of the Video Demand Tag'},
             {label: "country", key: "country", root: '${country}', helpText: 'The country of the current website'},
             {label: "timestamp", key: "timestamp", root: '${timestamp}', helpText: 'Current time in unix time'},
             {label: "device_id", key: "device_id", root: '${device_id}', helpText: 'The device id, we expect the user to provide a value'},
