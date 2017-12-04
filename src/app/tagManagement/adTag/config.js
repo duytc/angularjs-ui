9dc20abe3f1a0efb941be5de582a652972b3ee86
hieutran <dtag.dev1@gmail.com>
2017-12-04 17:32:05 +0700

    update label macros ad tag

--- src/app/tagManagement/adTag/config.js
+++ src/app/tagManagement/adTag/config.js
@@ -8,17 +8,17 @@
             inBanner: 2
         })
         .constant('MACROS_FOR_AD_TAG', [
-            {root: '${CACHEBUSTER}', helpText: "A unique code that ensures URLs are unique"},
-            {root: '${PAGE_URL}', helpText: "The url of the current page"},
-            {root: '${DOMAIN}', helpText: "The domain defined manually by user"},
-            {root: '${DECLARED_DOMAIN}', helpText: "The domain defined manually by user"},
-            {root: '${DETECTED_DOMAIN}', helpText: "The domain of the current website"},
-            {root: '${SLOT_ID}', helpText: "The ad slot ID of the current tag"},
-            {root: '${USER_AGENT}', helpText: "The user agent of the current browser"},
-            {root: '${COUNTRY}', helpText: "The country of the current website"},
-            {root: '${IP_ADDRESS}', helpText: "The IP address of the current website"},
-            {root: '${WIDTH}', helpText: "The Ad Slot width"},
-            {root: '${HEIGHT}', helpText: "The Ad Slot height"}
+            {root: '${CACHEBUSTER}', label: '${CACHEBUSTER}', helpText: "A unique code that ensures URLs are unique"},
+            {root: '${PAGE_URL}', label: '${PAGE_URL}', helpText: "The url of the current page"},
+            {root: '${DOMAIN}', label: '${DOMAIN}', helpText: "The domain defined manually by user"},
+            {root: '${DECLARED_DOMAIN}', label: '${DECLARED_DOMAIN}', helpText: "The domain defined manually by user"},
+            {root: '${DETECTED_DOMAIN}', label: '${DETECTED_DOMAIN}', helpText: "The domain of the current website"},
+            {root: '${SLOT_ID}', label: '${SLOT_ID}', helpText: "The ad slot ID of the current tag"},
+            {root: '${USER_AGENT}', label: '${USER_AGENT}', helpText: "The user agent of the current browser"},
+            {root: '${COUNTRY}', label: '${COUNTRY}', helpText: "The country of the current website"},
+            {root: '${IP_ADDRESS}', label: '${IP_ADDRESS}', helpText: "The IP address of the current website"},
+            {root: '${WIDTH}', label: '${WIDTH}', helpText: "The Ad Slot width"},
+            {root: '${HEIGHT}', label: '${HEIGHT}', helpText: "The Ad Slot height"}
         ])
         .run(function(editableOptions) {
             editableOptions.theme = 'bs3';
