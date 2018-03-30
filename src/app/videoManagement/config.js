197c1bc7464dca02fd54b242351fb13611933c9a
duytc <tranduy8683@gmail.com>
2018-03-30 17:21:26 +0700

    fix  bug: add demand sell price

--- src/app/videoManagement/config.js
+++ src/app/videoManagement/config.js
@@ -32,7 +32,8 @@
             // {label: "waterfall_id", key: "waterfall_id", root: '${waterfall_id}', helpText: 'Unique id of the Video Waterfall Tag'},
             // {label: "demand_tag_id", key: "demand_tag_id", root: '${demand_tag_id}', helpText: 'Unique id of the Video Demand Tag'},
             {label: "device_id", key: "device_id", root: '${device_id}', helpText: 'The device id, we expect the user to provide a value'},
-            {label: "device_name", key: "device_name", root: '${device_name}', helpText: 'The device name, we expect the user to provide a value'}
+            {label: "device_name", key: "device_name", root: '${device_name}', helpText: 'The device name, we expect the user to provide a value'},
+            {label: "demand_sell_price", key: "demand_sell_price", root: '${demand_sell_price}', helpText: 'The sell price of demand tag, only for "Fire On" is Impression in IVT Pixel'}
         ])
         .constant('VIDEO_MACROS_REFERENCE', {
 
